eee6d07a91e475bc332ac33eec8020eb3d45d213
Josh Stevens <joshstevens19@hotmail.co.uk>
2023-02-09 17:49:15 +0000

    cleanup: resolve some bad imports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { checkDAProof } from './check-da-proof';
-export { verifierFailedSubmissionsWatcher } from './watchers/failed-submissons.watcher';
+export { ClaimableValidatorError } from './claimable-validator-errors';
+export { TxValidatedFailureResult, TxValidatedResult, TxValidatedSuccessResult } from './db';
+export * from './watchers/stream.type';
 export { startDAVerifierNode } from './watchers/verifier.watcher';
--- src/watcher.ts
+++ src/watcher.ts
@@ -1,9 +1,5 @@
 import { startDAVerifierNode } from './';
 
-// const stream = (h: any) => {
-//   console.log('YES BABY', h);
-// };
-
 startDAVerifierNode().catch((error) => {
   console.error('DA verifier node failed to startup', error);
   process.exitCode = 1;
