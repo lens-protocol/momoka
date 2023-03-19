05af88c416de9f92e94ce03aed246501c49aa674
Josh Stevens <joshstevens19@hotmail.co.uk>
2023-03-19 21:14:08 +0000

    fix: remove annoying experimental warnings

--- src/runnable/da-verifier-node.runnable.ts
+++ src/runnable/da-verifier-node.runnable.ts
@@ -6,6 +6,25 @@ import { getParamOrExit } from '../common/helpers';
 import { startDAVerifierNode } from '../index';
 import { ethereumNode } from './ethereum-node-instance';
 
+// node please stop saying fetch is experimental - we know and we are not even using it :)
+const warningListeners = process.listeners('warning');
+if (warningListeners.length != 1) {
+  console.warn(
+    `expected 1 listener on the process "warning" event, saw ${warningListeners.length}`
+  );
+}
+
+if (warningListeners[0]) {
+  const originalWarningListener = warningListeners[0];
+  process.removeAllListeners('warning');
+
+  process.prependListener('warning', (warning) => {
+    if (warning.name != 'ExperimentalWarning') {
+      originalWarningListener(warning);
+    }
+  });
+}
+
 startDAVerifierNode(ethereumNode, getParamOrExit('DB_LOCATION_FOLDER_PATH')).catch((error) => {
   console.error('DA verifier node failed to startup', error);
   process.exitCode = 1;
