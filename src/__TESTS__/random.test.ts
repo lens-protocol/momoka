585d76ac3210542677e51880960dd9e961920448
Juan Garcia <juan@aave.com>
2023-04-06 10:01:41 +0200

    Remove unneeded test

--- src/__TESTS__/random.test.ts
+++ src/__TESTS__/random.test.ts
@@ -1,13 +1,7 @@
-import { getArweaveByIdAPI } from '../input-output/arweave/get-arweave-by-id.api';
 import { getBundlrByIdAPI } from '../input-output/bundlr/get-bundlr-by-id.api';
 import { LibCurlProvider } from '../input-output/lib-curl-provider';
 
 describe('random', () => {
-  it('getArweaveByIdAPI', async () => {
-    const txId = 'oWnpbkMpnGxMMnFDxnwxCQVhEK55jJeuiyLGUv2bSrk';
-    await getArweaveByIdAPI(txId);
-  });
-
   it('getBundlrByIdAPI', async () => {
     const txId = 'oWnpbkMpnGxMMnFDxnwxCQVhEK55jJeuiyLGUv2bSrk';
     await getBundlrByIdAPI(txId, { provider: new LibCurlProvider() });
