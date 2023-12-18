a1fbb16923842fae235917690894a7941ad98fb8
Paweł Lula <desfero@users.noreply.github.com>
2023-12-19 00:50:36 +0700

    Update submitter configuration (#33)

--- momoka-node/src/common/environment.ts
+++ momoka-node/src/common/environment.ts
@@ -4,7 +4,6 @@
 export enum Environment {
   POLYGON = 'POLYGON',
   MUMBAI = 'MUMBAI',
-  SANDBOX = 'SANDBOX',
 }
 
 /**
@@ -28,8 +27,6 @@ export const environmentToChainId = (environment: Environment): 137 | 80001 => {
       return 137;
     case Environment.MUMBAI:
       return 80001;
-    case Environment.SANDBOX:
-      return 80001;
     default:
       throw new Error('Invalid environment');
   }
@@ -47,8 +44,6 @@ export const environmentToLensHubContract = (environment: Environment): string =
       return '0xDb46d1Dc155634FbC732f92E853b10B288AD5a1d';
     case Environment.MUMBAI:
       return '0x60Ae865ee4C725cd04353b5AAb364553f56ceF82';
-    case Environment.SANDBOX:
-      return '0x7582177F9E536aB0b6c721e11f383C326F2Ad1D5';
     default:
       throw new Error('Invalid environment');
   }
